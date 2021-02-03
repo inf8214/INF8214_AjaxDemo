8cfacbab749e7aeceeaf3c1e8a31bab0512646dc
Mathieu Lemieux <mathieu@mathieulemieux.com>
2021-02-02 20:29:10 -0500

    Fichiers de base pour application Flask

---
+++ app.py
@@ -0,0 +1,32 @@
+import json, re
+from   flask      import Flask, render_template, request, send_file, jsonify
+from   pathlib    import Path
+from   flask_cors import CORS # Important si serveur Flask sur la même machine qu'un autre serveur!!
+
+
+# Setup Flask app.
+app = Flask(__name__)
+# app.config['SERVER_NAME'] = ''
+# app.config['APPLICATION_ROOT'] = ''
+CORS(app)
+
+
+# Routes
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+# Diseabling caching
+@app.after_request
+def add_header(r):
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
+
+
+# Run Flask!
+if __name__ == '__main__':
+    app.run(debug=True)
