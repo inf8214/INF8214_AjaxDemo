c7b642e40702c49ed42916415ec6df2895c277b4
Mathieu Lemieux <mathieu@mathieulemieux.com>
2021-03-15 13:13:44 -0400

    allo

--- app.py
+++ app.py
@@ -17,12 +17,27 @@ CORS(app)
 # Racine
 @app.route('/')
 def index():
+    # Retourne html à partir d'un template
     return render_template('index.html')
+
+# Route spécifique sans paramètres
+@app.route('/allo')
+def allo():
+    # Retourne html directement
+    return "<h1>Allo!</h1>"
+
+# Route spécifique avec paramètres
+@app.route('/allo/<nom>')
+def allo_nom(nom):
+    # Retourne html à partir d'un template en passant un paramètre
+    return render_template('allo.html', nom=nom)
 
 # Chemin d'accès complet
 @app.route('/<path:path>')
 def fichier(path):
+    # Retourne fichier static
     return app.send_static_file(path)
+
 
 
 
